d0017916f0784bb601a2fd747ff70b931973e9aa
Jjuandi <Juandiptcon@gmail.com>
2021-03-06 15:02:34 -0500

    Resolviendo problemas de rutas por cambio de Nombre Despok - Nimble

--- index.php
+++ index.php
@@ -2,25 +2,25 @@
 <html lang="es">
 	<head>
 		<meta charset="UTF-8">
-		<title>Despok</title>
-		<link rel="icon" href="Despok/pictures/nimble/nimble.png" />
+		<title>Nimble</title>
+		<link rel="icon" href="Nimble/pictures/nimble/nimble.png" />
 
 	<!-- Librerias Externas -->
-		<link rel="stylesheet" href="Despok/iconos/style.css">
-		<link rel="stylesheet" href="Despok/librerias/animate.css">
-		<script type="text/javascript" src="Despok/librerias/jquery-3.4.1.min.js"></script>
-		<script type="text/javascript" src="Despok/librerias/typed.js@2.0.js"></script>
+		<link rel="stylesheet" href="Nimble/iconos/style.css">
+		<link rel="stylesheet" href="Nimble/librerias/animate.css">
+		<script type="text/javascript" src="Nimble/librerias/jquery-3.4.1.min.js"></script>
+		<script type="text/javascript" src="Nimble/librerias/typed.js@2.0.js"></script>
 
 	<!-- Nuestros estilos Globales -->
-		<link rel="stylesheet" href="Despok/style/estilos_globales.css">
-		<link rel="stylesheet" href="Despok/style/fuentes.css">
+		<link rel="stylesheet" href="Nimble/style/estilos_globales.css">
+		<link rel="stylesheet" href="Nimble/style/fuentes.css">
 
 	<!-- Estilos Home -->
-		<link rel="stylesheet" href="Despok/style/home.css">
-		<link rel="stylesheet" href="Despok/style/brains.css">
+		<link rel="stylesheet" href="Nimble/style/home.css">
+		<link rel="stylesheet" href="Nimble/style/brains.css">
 
 	<!-- Scripts Home -->
-		<script type="text/javascript" src="Despok/script/home.js"></script>
+		<script type="text/javascript" src="Nimble/script/home.js"></script>
 
 	</head>
 	<body>
@@ -28,20 +28,20 @@
 			<header class="inicial-header">
 				<div class="contain-header">
 					<nav class="left">
-						<img src="Despok/pictures/nimble/nimble.png" alt="Despok">
+						<img src="Nimble/pictures/nimble/nimble.png" alt="Nimble">
 						<a href="#">Blog</a>
 						<a href="#">Herramientas</a>
 						<a href="#">Tienda</a>
 						<a href="#">| Terminos de Servicio</a>
 					</nav>
 					<nav class="right">
-						<a href="Despok/user/pages/register.php">Registrarme</a>
-						<a href="Despok/user/pages/login.php" class="inice-session">Iniciar sesión</a>
+						<a href="Nimble/user/pages/register.php">Registrarme</a>
+						<a href="Nimble/user/pages/login.php" class="inice-session">Iniciar sesión</a>
 					</nav>
 				</div>
 			</header>
-			<img src="Despok/pictures/server-img/question-mark-2492009_1920.jpg" alt="Background" class="background">
-			<img src="Despok/pictures/others/405-4053283.png" class="sombreado">
+			<img src="Nimble/pictures/server-img/question-mark-2492009_1920.jpg" alt="Background" class="background">
+			<img src="Nimble/pictures/others/405-4053283.png" class="sombreado">
 			<div class="content-group">
 				<section id="section1">
 					<div class="contentScript">
@@ -54,7 +54,7 @@
 						<p>Por ejemplo: <span class="spanM1"></span></p>
 						<div class="entrada">
 							<div class="icon">
-								<span class="despok_icon-search"></span>
+								<span class="Nimble_icon-search"></span>
 							</div>
 							<input type="text" placeholder="¿Qué quieres encontrar?">
 						</div>
@@ -66,8 +66,8 @@
 			<h2>Últimos post</h2>
 			<div class="content-blogs-all">
 				<div class="card">
-					<img src="Despok/posting/pictures/html.png" alt="¿Desarrollo web en latinoamérica?" class="background-post">
-					<img src="Despok/pictures/others/405-4053283.png" class="sombreado">
+					<img src="Nimble/posting/pictures/html.png" alt="¿Desarrollo web en latinoamérica?" class="background-post">
+					<img src="Nimble/pictures/others/405-4053283.png" class="sombreado">
 					<p class="title-posting">Esto es lo que debes aprender de desarrollo web ¡Para que no mueras de hambre!</p>
 					<div class="info-content-text">
 						<section>
@@ -76,14 +76,14 @@
 							<p>Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Autem veniam, fuga accusantium aperiam error, culpa itaque. Voluptate.</p>
 						</section>
 						<div class="data-posting">
-							<div class="dad"><span class="despok_icon-eye"></span>210</div>
-							<div class="dad"><span class="despok_icon-calendar"></span>Diciembre 20</div>
+							<div class="dad"><span class="Nimble_icon-eye"></span>210</div>
+							<div class="dad"><span class="Nimble_icon-calendar"></span>Diciembre 20</div>
 						</div>
 					</div>
 				</div>
 				<div class="card">
-					<img src="Despok/posting/pictures/170566.jpg" alt="¿Desarrollo web en latinoamérica?" class="background-post">
-					<img src="Despok/pictures/others/405-4053283.png" class="sombreado">
+					<img src="Nimble/posting/pictures/170566.jpg" alt="¿Desarrollo web en latinoamérica?" class="background-post">
+					<img src="Nimble/pictures/others/405-4053283.png" class="sombreado">
 					<p class="title-posting">¡Jennifer Lawrence! ¿Entrará a la nueva temporada de Grey's Anatomy?</p>
 					<div class="info-content-text">
 						<section>
@@ -92,14 +92,14 @@
 							<p>Lorem ipsum dolor, sit amet, consectetur adipisicing elit.</p>
 						</section>
 						<div class="data-posting">
-							<div class="dad"><span class="despok_icon-eye"></span>210</div>
-							<div class="dad"><span class="despok_icon-calendar"></span>Diciembre 20</div>
+							<div class="dad"><span class="Nimble_icon-eye"></span>210</div>
+							<div class="dad"><span class="Nimble_icon-calendar"></span>Diciembre 20</div>
 						</div>
 					</div>
 				</div>
 				<div class="card">
-					<img src="Despok/posting/pictures/people-2591693_1920.jpg" alt="¿Desarrollo web en latinoamérica?" class="background-post">
-					<img src="Despok/pictures/others/405-4053283.png" class="sombreado">
+					<img src="Nimble/posting/pictures/people-2591693_1920.jpg" alt="¿Desarrollo web en latinoamérica?" class="background-post">
+					<img src="Nimble/pictures/others/405-4053283.png" class="sombreado">
 					<p class="title-posting">Aumento del precio de los Quipitos ¡Genera protesta y caos! fuertes imágenes. 18+ </p>
 					<div class="info-content-text">
 						<section>
@@ -108,14 +108,14 @@
 							<p>Sáquenme de latinoamérica por favor :'v</p>
 						</section>
 						<div class="data-posting">
-							<div class="dad"><span class="despok_icon-eye"></span>210</div>
-							<div class="dad"><span class="despok_icon-calendar"></span>Diciembre 20</div>
+							<div class="dad"><span class="Nimble_icon-eye"></span>210</div>
+							<div class="dad"><span class="Nimble_icon-calendar"></span>Diciembre 20</div>
 						</div>
 					</div>
 				</div>
 				<div class="card">
-					<img src="Despok/posting/pictures/15891358002692.jpg" alt="¿Desarrollo web en latinoamérica?" class="background-post">
-					<img src="Despok/pictures/others/405-4053283.png" class="sombreado">
+					<img src="Nimble/posting/pictures/15891358002692.jpg" alt="¿Desarrollo web en latinoamérica?" class="background-post">
+					<img src="Nimble/pictures/others/405-4053283.png" class="sombreado">
 					<p class="title-posting">¡El Bas Buni compra Argentina y Venezuela! impactantes confesiones 18+ (no lo creerás)</p>
 					<div class="info-content-text">
 						<section>
@@ -124,8 +124,8 @@
 							<p>Lorem ipsum dolor sit Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Ratione, fuga.</p>
 						</section>
 						<div class="data-posting">
-							<div class="dad"><span class="despok_icon-eye"></span>210</div>
-							<div class="dad"><span class="despok_icon-calendar"></span>Diciembre 20</div>
+							<div class="dad"><span class="Nimble_icon-eye"></span>210</div>
+							<div class="dad"><span class="Nimble_icon-calendar"></span>Diciembre 20</div>
 						</div>
 					</div>
 				</div>
